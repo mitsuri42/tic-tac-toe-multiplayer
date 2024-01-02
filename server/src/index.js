6619af57be82b80695bc24d661514fa35b1c2e94
Dmitriy Pysarchuk <dmit.pysarchuk@gmail.com>
2024-01-02 17:28:11 +0200

    changws

--- server/src/index.js
+++ server/src/index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 const api_key = "-";
-const api_secret = "-";
+const api_secret = "--";
 const serverClient = StreamChat.getInstance(api_key, api_secret);
 
 app.post("/signup", async (req, res) => {
